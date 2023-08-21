8332b3ed28ebba914ebd3829aeeafc6823ad594c
SSK <SSK@192.168.1.71>
2023-08-21 23:40:43 +0530

    dddd

--- javaProj/src/javaProj/A.java
+++ javaProj/src/javaProj/A.java
@@ -2,8 +2,8 @@ package javaProj;
 
 public class A {
 
-	public A() {
-		// TODO Auto-generated constructor stub
+	public () {
+		// TODO  constructor stub
 	}
 
 	public static void main(String[] args) {
